809ec9aedcf62c69517d89046d64c3abbc6468d3
kostyamospan <kostya04042001@gmail.com>
2022-07-19 12:51:14 +0300

    multinetwork support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,23 +2,39 @@ import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 import "./tasks/timeTravel";
+import { HttpNetworkUserConfig, NetworksUserConfig, NetworkUserConfig } from "hardhat/types";
 
 dotenv.config();
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+const forkNetwork = process.env.FORK_NETWORK ?? 'bumperChainAlpha';
+
+console.log('Forking network is: ', forkNetwork);
+
+const configs: NetworksUserConfig = {
+  local: {
+    url: "http://localhost:8545",
+    chainId: 31337,
+  },
+  main: {
+    url: "https://eth-mainnet.g.alchemy.com/v2/xD6ljTKosJH4LJ0QJedVyf1uxMNYh8PE",
+    chainId: 1,
+  },
+  bumperChainAlpha: {
+    url: "https://alpha.rpc.bumper-dao.com/rpc",
+    chainId: 12345,
+  },
+}
+
 const config: HardhatUserConfig = {
   networks: {
-    
-    local: {
-      url: "http://localhost:8545",
-      chainId: 31337,
-    },
+    ...configs,
     hardhat: {
       chainId: 31337,
       forking: {
-        url: "https://eth-goerli.alchemyapi.io/v2/-ArWHOOYvAQ-o_Tlr08aE2GQX5hmBuvf",
+        url: (configs[forkNetwork] as HttpNetworkUserConfig)?.url ?? '',
       }
     }
   },
